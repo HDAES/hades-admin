b2efbb39214b8c81228aa33c946257cb47a0fd4e
hades <1005710179@qq.com>
2022-10-13 00:27:23 +0800

    ✨特性(模块1): ✨特性(模块1): ✨特性(模块1): ✨特性: ♻️重构: 格式化代码

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,19 +3,21 @@
     <el-button type="primary">Primary1</el-button>
     <DarkModeToggle />
     <SwitchTheme />
-    <SvgIcon size="14" name="quanxian" />
+    <SvgIcon size="14"
+             name="quanxian" />
     <SwitchLanguage />
     <p>12312</p>
-    <p>{{ $t('sys.hello') }}</p>
+    <p>{{ $t('sys.hello') }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style>
-a {
+a {  background-color: #f0f2f5;
   width: 100%;
   font-weight: bold;
-  background-color: #f0f2f5;
+
 }
 </style>
