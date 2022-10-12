2e5bff8161643e72ebc9ca75a0afe67786c16a44
hades <1005710179@qq.com>
2022-10-13 00:01:57 +0800

    ✨特性: ♻️重构: 格式化代码

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,7 +8,6 @@
     <SwitchLanguage />
     <p>12312</p>
     <p>{{ $t('sys.hello') }}
-
     </p>
   </div>
 </template>
@@ -17,3 +16,11 @@
 import { useI18n } from '@/hooks/web/useI18n'
 const { t } = useI18n()
 </script>
+
+<style>
+a{
+  width: 100%;
+  background-color: #f0f2f5;
+  font-weight: bold;
+}
+</style>
